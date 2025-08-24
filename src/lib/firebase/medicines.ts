94b93e5c5ca64a538a910aabbd3abb7c01f816dd
G K SANJAY KUMAAR CSE <gksanjaykumaar.cse2024@citchennai.net>
2025-08-24 04:07:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/medicines.ts
+++ src/lib/firebase/medicines.ts
@@ -9,7 +9,7 @@ import {
     query,
     orderBy
 } from 'firebase/firestore';
-import { db } from './firebase';
+import { db } from '../firebase';
 import type { Medicine, NewMedicine, UpdateMedicine } from '@/types/medicine';
 
 const medicinesCollection = collection(db, 'medicines');
