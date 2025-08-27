dd96610dfb99d4500a14a2749e8ed0a050d1f4a1
G K SANJAY KUMAAR CSE <gksanjaykumaar.cse2024@citchennai.net>
2025-08-27 06:31:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -56,7 +56,7 @@ export default function CartPage() {
             <Card>
               <CardHeader>
                 <CardTitle>Cart Items ({items.length})</CardTitle>
-              </Header>
+              </CardHeader>
               <CardContent>
                 <Table>
                   <TableHeader>
